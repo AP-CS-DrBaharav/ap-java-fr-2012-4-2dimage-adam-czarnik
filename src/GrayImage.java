105204ab0567dc5a43490d2117601112cb29f26c
adam-czarnik <aczarnik@2018.kehillah.org>
2018-01-24 12:50:32 -0800

    done

--- src/GrayImage.java
+++ src/GrayImage.java
@@ -22,16 +22,21 @@ public class GrayImage
   /** @return the total number of white pixels in this image.
    *    Postcondition: this image has not been changed.
    */
-  public int countWhitePixels()
+  public int countWhitePixels() {
 
-          
-          
-          
-          
-          
-          
-          
-          
+      int cnt = 0;
+      int rows = pixelValues.length;
+      int cols = pixelValues[0].length;
+      
+      for (int i=0; i<rows; ++i) {
+          for (int j=0; j<cols; j++) {
+              if (pixelValues[i][j] == 255) {
+                  cnt++;
+              }
+          }
+      }
+        return cnt;
+  }
 
   // Part (b)
 
@@ -40,13 +45,21 @@ public class GrayImage
    * Resulting values that would be less than BLACK are replaced by BLACK.
    * Pixels for which there is no pixel at position (row + 2, col + 2) are unchanged.
    */
-  public void processImage()
-
+  public void processImage() {
           
-          
-          
-          
-          
+      int rows = pixelValues.length;
+      int cols = pixelValues[0].length;
+      for (int i=0; i<rows-2; ++i) {
+          for (int j=0; j<cols-2; j++) {
+              if (pixelValues[i][j]-pixelValues[i+2][j+2] < 0) {
+                  pixelValues[i][j] = 0;
+              } else {
+                  pixelValues[i][j] = pixelValues[i][j]-pixelValues[i+2][j+2];
+              }
+          }
+      }
+      
+  }
 
   public String toString()
   {
